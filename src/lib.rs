d7d79273d233a536111dffb36086314667656ffa
Andrey Cherkashin <with.out@me.com>
2019-09-07 14:08:52 -0700

    feat: Initial implementation of kernel space allocator shim

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,11 @@
 #![no_std]
 #![deny(warnings)]
 
-pub mod ffi;
+mod ffi;
 use crate::ffi as kern;
 use bk_primitives as raw;
 
 use core::alloc::{GlobalAlloc, Layout};
-
-#[cfg(all(any(
-    target_arch = "x86",
-    target_arch = "arm",
-    target_arch = "mips",
-    target_arch = "powerpc",
-    target_arch = "powerpc64",
-    target_arch = "asmjs",
-    target_arch = "wasm32"
-)))]
-const MIN_ALIGN: usize = 8;
-#[cfg(all(any(
-    target_arch = "x86_64",
-    target_arch = "aarch64",
-    target_arch = "mips64",
-    target_arch = "s390x",
-    target_arch = "sparc64"
-)))]
-const MIN_ALIGN: usize = 16;
 
 pub struct BkAllocator;
 
@@ -36,29 +17,27 @@ impl BkAllocator {
             flags,
         ) as *mut u8
     }
-    unsafe fn alloc_aligned(&self, _layout: Layout, _flags: i32) -> *mut u8 {
-        unimplemented!();
-    }
 }
 
 unsafe impl GlobalAlloc for BkAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         let flags = kern::M_WAITOK as i32;
-        if layout.align() <= MIN_ALIGN && layout.align() <= layout.size() {
-            self.alloc(layout, flags)
-        } else {
-            self.alloc_aligned(layout, flags)
-        }
+        self.alloc(layout, flags)
     }
     unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 {
         let flags = kern::M_WAITOK as i32 | kern::M_ZERO as i32;
-        if layout.align() <= MIN_ALIGN && layout.align() <= layout.size() {
-            self.alloc(layout, flags)
-        } else {
-            self.alloc_aligned(layout, flags)
-        }
+        self.alloc(layout, flags)
     }
     unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
         kern::free(ptr as *mut raw::c_void, &mut kern::M_DEVBUF[0])
+    }
+    unsafe fn realloc(&self, ptr: *mut u8, _layout: Layout, new_size: usize) -> *mut u8 {
+        let flags = kern::M_WAITOK as i32;
+        kern::realloc(
+            ptr as *mut raw::c_void,
+            new_size as raw::c_ulong,
+            &mut kern::M_DEVBUF[0],
+            flags,
+        ) as *mut u8
     }
 }
